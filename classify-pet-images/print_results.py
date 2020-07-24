cb7032d8843c10955bc52652da4d6e8c019820af
Wesley Jacoby <wesleyjacoby@gmail.com>
2020-07-24 08:47:22 +0200

    Refactor print statements to f-strings

--- classify-pet-images/print_results.py
+++ classify-pet-images/print_results.py
@@ -76,7 +76,7 @@ def print_results(results_dic, results_stats_dic, model,
             # Pet Image Label is a Dog - Classified as NOT-A-DOG -OR- 
             # Pet Image Label is NOT-a-Dog - Classified as a-DOG
             if sum(results_dic[key][3:]) == 1:
-                print("Real: {:>26}   Classifier: {:>30}".format(results_dic[key][0], results_dic[key][1]))
+                print(f'Real: {results_dic[key][0]:>26}   Classifier: {results_dic[key][1]:>30}')
 
     # IF print_incorrect_breed == True AND there were dogs whose breeds 
     # were incorrectly classified - print out these cases
@@ -87,4 +87,4 @@ def print_results(results_dic, results_stats_dic, model,
         for key in results_dic:
             # Pet Image Label is-a-Dog, classified as-a-dog but is WRONG breed
             if (sum(results_dic[key][3:]) == 2 and results_dic[key][2] == 0 ):
-                print("Real: {:>26}   Classifier: {:>30}".format(results_dic[key][0], results_dic[key][1]))
+                print(f'Real: {results_dic[key][0]:>26}   Classifier: {results_dic[key][1]:>30}')
